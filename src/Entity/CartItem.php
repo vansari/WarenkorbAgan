782c99899d8982cc38fad9da0a2be8c48fdf1957
vansari <kurt.junker@posteo.de>
2023-04-15 16:46:33 +0200

    CartItem Entity and Tests

---
+++ src/Entity/CartItem.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CartItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CartItemRepository::class)]
+class CartItem
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Product $product = null;
+
+    #[ORM\Column]
+    private int $quantity = 0;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(Product $product): self
+    {
+        $this->product = $product;
+        $this->setQuantity(1);
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function addOne(): self
+    {
+        $this->setQuantity(($this->getQuantity() + 1));
+        return $this;
+    }
+
+    public function subtractOne(): self
+    {
+        if (0 < $this->getQuantity()) {
+            $this->setQuantity(($this->getQuantity() - 1));
+        }
+
+        return $this;
+    }
+}
